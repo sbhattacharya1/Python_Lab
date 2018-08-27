1857b8168fde12f14ccf4759dfa99a65cbeecc2c
Sarani Bhattacharya <42635843+sbhattacharya1@users.noreply.github.com>
2018-08-27 03:32:26 -0400

    Create fibonacii_series.py

---
+++ fibonacii_series.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# @Program: To Print Fibonacii series upto N number
+# @Author: Sarani Bhattacharya
+from __future__ import print_function
+from sys import argv
+
+def  fibonacii_series(n):
+    x,y=0,1
+    print ("Fibonacii Series(%d):" %(n))
+    print (" %d " %(x), end =' ')
+    for i in range(1,n):
+            print (" %d " %(y), end =' ')
+            z=x+y
+            x=y
+            y=z
+    return;
+#Main 
+if len(argv) !=2:
+    exit(1)
+n=int(argv[1])
+fibonacii_series(n)
