279b0f285fd1ce2773119d35728f67a86de432b0
Sarani Bhattacharya <42635843+sbhattacharya1@users.noreply.github.com>
2019-01-11 07:31:39 -0600

    Update factorial_recursive.py

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python
+# @Program: To Print Factorial of a number N, function in recursive nature
+# @Author: Sarani Bhattacharya
+
+#Recursive function for factorial
 def fact(n):
     if n<=1:
         return 1 
     else: 
         return n*fact(n-1)
 
-num=int(input("Please enter a number: "))
-print("Factorial of {} is :{}".format(num,fact(num)))
+
+num=int(input("Please enter a number: "))                           #Get the number
+print("Factorial of {} is :{}".format(num,fact(num)))               #Print the factorial
